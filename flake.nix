a8d86848f71dd77fcfb47b5925c46b291c7b7a28
Adrian Pistol <vifino@posteo.net>
2026-04-08 17:59:05 +0200

    chore(flake,Cargo.lock,actions): Update to NixOS 25.11 and Rust 1.94.1.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -35,12 +35,12 @@
         };
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            (rust-bin.stable."1.86.0".default.override {
+            (rust-bin.stable."1.94.1".default.override {
               extensions = ["llvm-tools-preview"];
             })
             cargo-bloat
             cargo-llvm-cov
-            llvmPackages_19.bintools
+            llvmPackages.bintools
           ];
         };
 
